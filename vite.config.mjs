9ce166bfc47da4d57140b64e6b0c125772c8c3af
CHAN YH <chan_yh@icloud.com>
2025-08-18 09:17:58 +0800

    Commit

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,9 @@
-// vite.config.mjs
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "build", // 👈 change from default 'dist'
+  },
 });
