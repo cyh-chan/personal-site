d2f3a5a15d88e9e0caccc5ca0bdcf0d26700ddaa
CHAN YH <chan_yh@icloud.com>
2025-08-18 09:29:12 +0800

    Commit

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // change from 'dist' to 'build'
+    outDir: "build", // must match Cloudflare Pages build output
   },
-  base: "/",
+  base: "/", // ensures paths are correct
 });
