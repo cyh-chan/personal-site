bc05e44c08fa7591d3320c01e07720238fb7cff4
CHAN YH <chan_yh@icloud.com>
2025-08-18 10:06:42 +0800

    Commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,42 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import Markdown from 'markdown-to-jsx';
+import React from "react";
+import { Link } from "react-router-dom";
+import Markdown from "markdown-to-jsx";
+import Main from "../layouts/Main";
+import Personal from "../components/Stats/Personal";
 
-import Main from '../layouts/Main';
-
-import Personal from '../components/Stats/Personal';
+import { markdown } from "../data/about";
 
 const About = () => {
-  const [markdown, setMarkdown] = useState('');
-
-  useEffect(() => {
-    import('../data/about.md')
-      .then((res) => {
-        fetch(res.default)
-          .then((r) => r.text())
-          .then(setMarkdown);
-      });
-  });
-
-  const count = markdown.split(/\s+/)
-    .map((s) => s.replace(/\W/g, ''))
+  const count = markdown
+    .split(/\s+/)
+    .map((s) => s.replace(/\W/g, ""))
     .filter((s) => s.length).length;
 
   return (
-    <Main
-      title="About"
-      description="Learn about Chan YH"
-    >
+    <Main title="About" description="Learn about Chan YH">
       <article className="post markdown" id="about">
         <header>
           <div className="title">
-            <h2><Link to="/about">About Me</Link></h2>
+            <h2>
+              <Link to="/about">About Me</Link>
+            </h2>
             <p>(in about {count} words)</p>
           </div>
         </header>
-        <Markdown>
-          {markdown}
-        </Markdown>
+        <Markdown>{markdown}</Markdown>
         <Personal />
       </article>
     </Main>
